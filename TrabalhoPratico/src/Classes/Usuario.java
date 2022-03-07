849f386f6b4c8f72a5743ea7ce7d2a11e2a5c79e
Lucas <lucasfs1007@gmail.com>
2022-03-07 16:08:37 -0300

    Atualizando a criação dos métodos de Usuario...faltando finalizar alguns métodos ainda pro futuro

--- TrabalhoPratico/src/Classes/Usuario.java
+++ TrabalhoPratico/src/Classes/Usuario.java
@@ -1,16 +1,18 @@
 import java.util.Date;
 import java.util.Scanner;
-
+import java.util.Scanner;
 public class Usuario extends Pessoa {
 	//Atributos
 	private String email;
 	private String senha;
+	private String verificaSenha;
 	private Scanner ler = new Scanner (System.in); // Utilizarei para fazer os metodos.
 	
-	public Usuario( String e, String s,String n,double t,double rg,String c,Date nascimento,int anos) {
+	public Usuario( String e, String s,String vs,String n,double t,double rg,String c,Date nascimento,int anos) {
 		super();
 		email=e;
 		senha=s;
+		verificaSenha=vs;
 		nome=n;
 		telefone=t;
 		identidade=rg;
@@ -26,6 +28,48 @@ public class Usuario extends Pessoa {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public String getVerificaSenha() {
+		return verificaSenha;
+	}
+	public void setVerificaSenha(String verificaSenha) {
+		this.verificaSenha = verificaSenha;
+	}
+	//Metodos
+	public void cadastrarUsuario() { //Cadastrando email e senha, vista que os outros dados devem ser coletados na classe pessoa
+		do { //Validando o email.
+			System.out.print("Insira seu email: ");
+			setEmail(ler.nextLine());
+		}while(email.length==0 || email.contains==('@')); //Email tem que ter @ e nao pode ser vazia
+		do {
+			System.out.print("Insira a senha: ");
+			setSenha(ler.nextLine()); 
+			System.out.print("Digite novamente a senha para se confirmar: ");
+			setverificaSenha(ler.nextLine());
+				if(senha != verificaSenha) {
+					System.out.print("As senhas não correspondem.");
+				}else {
+					if(senha.length()==0) {
+						System.out.print("A senha não pode ser nula. Tente novamente");
+					}
+		}while(senha !=verificaSenha || (senha.length()==0)); //As senha e verifificacao devem ser iguais e nao nulas
+	}
+	public void editarUsuario() {
+		
+	}
+	public void deletarUsuario() { //Zerando email e senha do usuario
+		setEmail(null);
+		setSenha(null);
+	}
+	public void atualizarUsuario() {
+		
+	}
+	public void listarUsuario() {
+		
+	}
+	public void buscarUsuario() {
+		
+	}
 	}
 	
 
