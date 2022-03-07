aaae32a0d2e91f4484ae18b65db469287c96c774
Lucas Felipe Soares <88278278+lucasfs1007@users.noreply.github.com>
2022-03-07 10:49:02 -0300

    Add files via upload
    Classe pessoa com criação dos métodos e construtores (ainda há problemas no cadastrar pessoas)

--- TrabalhoPratico/src/Classes/Pessoa.java
+++ TrabalhoPratico/src/Classes/Pessoa.java
@@ -1,12 +1,17 @@
 import java.util.Date;
-
+import java.util.Scanner;
 public class Pessoa {
+	//Atributos
 	private String nome;
 	private double telefone;
 	private double identidade;
 	private String cpf;
 	private Date dataNascimento;
 	private int idade;
+	private int opcaoMenu; //Utilizarei para o menu de ediçoes.
+	private Usuario usuario;
+	private Scanner ler = new Scanner (System.in); // Utilizarei para fazer os metodos.
+	//Construtor
 	public Pessoa(String n,double t, double rg, String c, Date nascimento, int anos) {
 		nome=n;
 		telefone=t;
@@ -15,6 +20,7 @@ public class Pessoa {
 		dataNascimento=nascimento;
 		idade=anos;
 	}
+	//Gets e Sets
 	public String getNome() {
 		return nome;
 	}
@@ -50,6 +56,74 @@ public class Pessoa {
 	}
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		usuario.cadastrarUsuario();
+	}
+	//Metodos
+	public void cadastrarPessoa() {  //Cadastro dos dados basicos das pessoas que será herdado pra classe usuario.
+		System.out.print("\nDigite seu nome:");
+		setNome(ler.nextLine());
+		System.out.print("Digite seu CPF:");
+		setCpf(ler.nextLine());
+		System.out.print("Digite seu telefone:");
+		setTelefone(ler.nextDouble());
+		System.out.println("Digite seu RG:");
+		setIdentidade(ler.nextDouble());
+		System.out.print("Digite sua idade: ");
+		setIdade(ler.nextInt());
+		System.out.print("Digite sua data de Nascimento: ");
+		setDataNascimento(ler.nextDate()); //Preciso descobrir como se faz pra datas.
+		setUsuario(usuario);// Chama funcao que edita o usuario
+		System.out.println("\nPressione Enter para continuar...");
+		ler.nextLine();
+	}
+	public void editarPessoa() { //Editar os dados da pessoa em questão, e consequentemente alterando em usuario
+		System.out.println("Digite a opcao que se deseja editar: \n");
+		System.out.println(" 1 - Nome \n");
+		System.out.println(" 2 - Telefone \n");
+		System.out.println(" 3 - Idade \n");
+		System.out.println(" 4 - Email \n");
+		System.out.println(" 5 - Senha \n");
+		opcaoMenu = ler.nextInt(); //Lendo a opcao desejada pelo usuario
+		if(opcaoMenu < 1 ||opcaoMenu>5) { //Não existe essas opcoes.
+			System.out.println("Ops, não existe essa opcao. \n");
+		}else {
+			if(opcaoMenu==1) {
+				System.out.print("Digite seu nome:");
+				setNome(ler.nextLine());
+				System.out.println("Nome alterado com sucesso.");
+			}else if(opcaoMenu==2) {
+				System.out.print("Digite seu novo telefone:");
+				setTelefone(ler.nextInt());
+				System.out.println("Telefone alterado com sucesso.");
+			}else if(opcaoMenu==3) {
+				System.out.println("Digite sua idade:");
+				setIdade(ler.nextInt());
+				System.out.print("Idade alterada com sucesso.");	
+			}else if(opcaoMenu==4) {
+				System.out.print("Digite seu Email:");
+				Usuario.setEmail(ler.nextLine());
+				System.out.println("Email alterado com sucesso.");
+			}else if(opcaoMenu==5) {
+				System.out.print("Digite sua Senha:");
+				Usuario.setSenha(ler.nextLine());
+				System.out.println("Senha alterada com sucesso.");
+			}
+		}
+	}
+	public void deletaPessoa() {// Zerando todos os dados da pessoa/Usuario
+		setCpf(null);
+		setNome(null);
+		setIdentidade(null); //Preciso descobrir como se faz pra esses dois.
+		setTelefone(null);
+		setUsuario(null);
+		System.out.println("\nUsuario deletado com sucesso.");
+		System.out.println("\nPressione Enter para continuar...");
+		ler.nextLine();
 	}
 
 }
