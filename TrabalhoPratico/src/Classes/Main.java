7812ae38ab414f92dd5f21732ab96556d5ec734b
jfnas <jfnas@DESKTOP-E14BUGV>
2022-03-08 19:15:25 -0300

    Criação dos menus

--- TrabalhoPratico/src/Classes/Main.java
+++ TrabalhoPratico/src/Classes/Main.java
@@ -1,11 +1,31 @@
 package Classes;
 import Classes.*; // O "*" importa todas as classes
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Menu
+		
+		char chamarMenu, chamarMenuUsuario;
+		
+		do {
+			chamarMenu = menu();
+			switch(chamarMenu) {
+			case'1': //Exibir menu usuário
+				chamarMenuUsuario = menuUsuario();
+				switch(chamarMenuUsuario) {
+				case'1': // Cadastro de usuário
+					usuario[0].cadastrUsuario(usuario)
+				}
+			}
+		}
+		
+		System.out.println("---------------------------------");
+		System.out.println(" Sistema de aluguel de bicicleta ");
+		System.out.println("---------------------------------");
+		
 	}
 
 }
