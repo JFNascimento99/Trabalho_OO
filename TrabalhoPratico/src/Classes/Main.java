c3a207abe5ba5c38e5a4daee0851e5179ac0360a
Lucas Felipe Soares <88278278+lucasfs1007@users.noreply.github.com>
2022-03-13 15:38:00 -0300

    Add files via upload
    Adicionando a base da matriz de bicicletas, cadastrando 10 bicicletas tradicionais e 10 bicicletas elétricas.

--- TrabalhoPratico/src/Classes/Main.java
+++ TrabalhoPratico/src/Classes/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Definição dos Bicicletários
+		//Definicao dos Bicicletarios
 		Bicicletario[] bicicletarios = new Bicicletario[] {
 				new Bicicletario("Asa Norte", "SQN 116"),
 				new Bicicletario("Asa Norte", "SQN 114"),
@@ -20,7 +20,33 @@ public class Main {
 				new Bicicletario("Asa Sul", "SQS 108"),
 		};
 		
-		
+		//Definicao dos tipos de bicicleta
+		tipoBicicleta[][] bicicletas = new bicicletas[][] { //Matriz pq e um vetor p bicicletario e outro para os dois tipos de bicileta que teremos
+			//cadastro das 10 bicicletas tradicionais
+			new bicicletas("Bicicleta 1", "Tradicional");
+			new bicicletas("Bicicleta 2", "Tradicional");
+			new bicicletas("Bicicleta 3", "Tradicional");
+			new bicicletas("Bicicleta 4", "Tradicional");
+			new bicicletas("Bicicleta 5", "Tradicional");
+			new bicicletas("Bicicleta 6", "Tradicional");
+			new bicicletas("Bicicleta 7", "Tradicional");
+			new bicicletas("Bicicleta 8", "Tradicional");
+			new bicicletas("Bicicleta 9", "Tradicional");
+			new bicicletas("Bicicleta 10", "Tradicional");
+			//Cadastro das 10 bicicletas eletricas
+			new bicicletas("Bicicleta 11", "Eletrica");
+			new bicicletas("Bicicleta 12", "Eletrica");
+			new bicicletas("Bicicleta 13", "Eletrica");
+			new bicicletas("Bicicleta 14", "Eletrica");
+			new bicicletas("Bicicleta 15", "Eletrica");
+			new bicicletas("Bicicleta 16", "Eletrica");
+			new bicicletas("Bicicleta 17", "Eletrica");
+			new bicicletas("Bicicleta 18", "Eletrica");
+			new bicicletas("Bicicleta 19", "Eletrica");
+			new bicicletas("Bicicleta 20", "Eletrica");
+				
+		}
+		//Devemos criar uma lista de pessoas e usuarios previamente cadastrados.
 		/*
 		//Menu
 		
