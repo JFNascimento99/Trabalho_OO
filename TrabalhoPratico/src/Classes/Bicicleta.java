28cb00d9742122d6daa26accbf542395cd1f4ea2
Lucas Felipe Soares <88278278+lucasfs1007@users.noreply.github.com>
2022-03-16 18:54:14 -0300

    Add files via upload
    Adicionando o esqueleto de alguns métodos para Bicicleta

--- TrabalhoPratico/src/Classes/Bicicleta.java
+++ TrabalhoPratico/src/Classes/Bicicleta.java
@@ -7,7 +7,8 @@ public class Bicicleta {
 	//Bicicleta faz parte de pedido de forma obrigatoria...composição pois sem bicicleta sem pedido.
 	//Atributos de bicicleta
 	
-	public String Bicicletario[];
+	public String Bicicleta[][]; //Um e a posicao em relacao aos bicicletarios e outro em relacao as bicicletas
+	
 	private String tipoBicicleta;
 	private String nomeBicicleta;
 	private boolean disponibilidadeBicicleta;
@@ -58,66 +59,105 @@ public class Bicicleta {
 	public void verBicicletas() { //Listar/ ver as bicicletas cadastradas
 		
 	}
-	public void buscarBicicletario() { 
-		  //buscar todos bicicletarios cadastrados...talvez fosse mais interessante o usuario digitar, agt comparar texto e mostrar se achar. 
-		  //Precisamos que cada bicicletario seja armazenado em uma posicao do vetor(por isso que vai ta dando esse erro ali embaixo por enquanto) 
-		  //Seria legal fora o horario, mostrar as bicicletas cadastradas tbm. 
+	public void buscarBicicletas() { 
+		  //buscar todas bicicletas cadastradas...talvez fosse mais interessante o usuario digitar, agt comparar texto e mostrar se achar. 
+		  //Precisamos que cada bicicleta seja armazenado em uma posicao do vetor(por isso que vai ta dando esse erro ali embaixo por enquanto)
+		
+		String buscaBicicleta = ""; //Utilizaremos para comparar oq o usuario digitou com o que estava armazenado
+		Boolean encontradoBicicleta = false; //Para vermos se achamos a bicicleta procurada.
+		Scanner resposta = new Scanner(System.in); //Leitura de dados
+		
+		//Logica de busca que usei no meu TP1
+		for(int a=0; a<=100; a++){ //Passando por todo vetor de bicicletarios 
+			   System.out.println("Digite o bicicletario que se deseja buscar as Bicicletas:"); 
+			   procuraBicicletario = resposta.next(); //Lendo o bicicletario desejado. 
+			    if(Bicicletario[a].equals(procuraBicicletario)) {  //Devemos arrumar e ter esse vetor de bicicletarios. 
+			     System.out.println("Na posicao: " + a + " foi encontrado " + Bicicletario[a]); //Mostrando os dados que foram achados para esse bicicletario 
+			     encontrado = true; //A palavra digitada foi igual a alguma armazenada, logo existe o bicicletario 
+			     for(int b=0; b<=100; b++) { //Passando agora por todo vetor de bicicletas.
+			    	 System.out.println("Digite a bicicleta que se deseja buscar:");
+			    	 buscaBicicleta = resposta.next(); //Lendo a bicicleta que o usuario quer buscar (se pah ler o numero dela é melhor)
+			    	 if(bicicleta[a][b].equals(buscaBicicleta)) { //Comparando o que foi digitado com o que tinhamos armazenado
+			    		 System.out.println("Na posicao: " + b + "foi encontrado " + bicicleta[a][b]);
+			    		 enontradoBicicleta = true;
+			    	 }
+			    	 if(!encontradoBicicleta) {
+			    		 System.out.println("Na estacao escolhida, nao ha a bicicleta buscada");
+			    	 }
+			     }
+			    } 
+			    
+			  } 
+			  if(!encontrado) { //Caso oq for digitado nao for igual ao que tinhamos armazenado 
+			   System.out.println("Este bicicletario nao existe ainda "); 
+			  } 
 		   
-		  String procuraBicicletario =""; //Utilizaremos para comparar o que o usuario digitou com o que temos armazenado. 
-		  Boolean encontrado = false; //Para vermos se achamos o bicicletario procurado 
-		  Scanner resposta = new Scanner(System.in); //Leitura de dados  
-		  
-		  //Logica de busca que usei no meu TP1. 
-		  for(int a=0; a<=100; a++){ //Passando por todo vetor de bicicletarios 
-		   System.out.println("Digite o bicicletario que se deseja buscar:"); 
-		   procuraBicicletario = resposta.next(); //Lendo o bicicletario desejado. 
-		    if(Bicicletario[a].equals(procuraBicicletario)) {  //Devemos arrumar e ter esse vetor de bicicletarios. 
-		     System.out.println("Na posicao: " + a + " foi encontrado " + Bicicletario[a]); //Mostrando os dados que foram achados para esse bicicletario 
-		     encontrado = true; //A palavra digitada foi igual a alguma armazenada, logo existe o bicicletario 
-		    } 
-		    
-		  } 
-		  if(!encontrado) { //Caso oq for digitado nao for igual ao que tinhamos armazenado 
-		   System.out.println("Este bicicletario nao existe ainda "); 
-		  } 
-		   
+		
 		 } 
-		 public void listarBicicletarios() { 
-		  //Listar todos os bicicletrarios cadastrados...esses pre cadastrados e os que o usuario adicionar. 
-		  //Mostrar somente os bicicletarios mesmo. 
-		  //Colocando as que estavam na main já. 
-		   
-		  //Bicicletarios da Asa norte 
-		  System.out.println("Asa Norte: SQN 116 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Norte: SQN 114"); 
-		  System.out.println("Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Norte: SQN 112 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Norte: SQN 110"); 
-		  System.out.println("Horarios:Atendimento 24 horas. "); 
-		  System.out.println("Asa Norte: SQN 108 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  //Bicicletarios da Asa Sul 
-		   
-		  System.out.println("Asa Sul: SQS 116 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Sul: SQS 114 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Sul: SQS 112 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Sul: SQS 110 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
-		   
-		  System.out.println("Asa Sul: SQS 108 "); 
-		  System.out.println( "Horarios: Atendimento 24 horas. "); 
+		 public void listarBicicletas() { 
+		  //Listar todas as bicicletas cadastrados...essas pre cadastradas e os que o usuario adicionar. 
+			 System.out.println("Bicicleta 1"); 
+			 System.out.println( "Tradiconal "); 
+			   
+			 System.out.println("Bicicleta 2"); 
+			 System.out.println("Tradicional "); 
+			   
+			 System.out.println("Bicicleta 3 "); 
+			 System.out.println( "Tradicional "); 
+			   
+			 System.out.println("Bicicleta 4"); 
+			 System.out.println("Tradicional "); 
+			 
+			 System.out.println("Bicicleta 5 "); 
+			 System.out.println( "Tradicional "); 
+			   
+			   
+			 System.out.println("Bicicleta 6 "); 
+			 System.out.println( "Tradicional "); 
+			   
+			 System.out.println("Bicicleta 7 "); 
+			 System.out.println( "Tradicional "); 
+			   
+			 System.out.println("Bicicleta 8 "); 
+			 System.out.println( "Tradicional "); 
+			   
+			 System.out.println("Bicicleta 9"); 
+			 System.out.println( "Tradicional "); 
+			   
+			 System.out.println("Bicicleta 10 "); 
+			 System.out.println( "Tradicional "); 
+			 
+			 //Bicicletas Eletricas
+			   
+			  System.out.println("Bicicleta 11 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 12"); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 13 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 14 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 15 "); 
+			  System.out.println( "Eletrica "); 
+			  
+			  System.out.println("Bicicleta 16 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 17 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 18 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 19 "); 
+			  System.out.println( "Eletrica "); 
+			   
+			  System.out.println("Bicicleta 20 "); 
+			  System.out.println( "Eletrica "); 
 		 }
 
 }
