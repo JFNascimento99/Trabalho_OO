04de01c968aa5c3e283d15d4260b7eba9f5f9aca
Lucas <lucasfs1007@gmail.com>
2022-03-26 10:02:39 -0300

    Atualizando os métodos de forma de pagamento e corrigindo os erros

--- TrabalhoPratico/src/Classes/FormaDePagamento.java
+++ TrabalhoPratico/src/Classes/FormaDePagamento.java
@@ -7,13 +7,13 @@ public class FormaDePagamento {
 	//Forma de pagamento está agregada a usuario...faz parte de usuario
 	//Atributos
 	private String bandeiraCartao;
-	private double numeroCartao;
-	private int codigoSeguranca;
+	private String numeroCartao;
+	private String codigoSeguranca;
 	private String tipoPagamento;
 	//Variaveis extra pra implementar algumas logicas
 	String respostaBandeira;
-	double respostaNumero;
-	int respostaSeguranca;
+	String respostaNumero;
+	String respostaSeguranca;
 	String respostaPagamento;
 	Boolean validaBandeira = true;
 	Boolean validaPagamento = true;
@@ -23,7 +23,7 @@ public class FormaDePagamento {
 	static Scanner ler = new Scanner(System.in);
 	
 	//Metodo construtor da Forma de Pagamento
-	public FormaDePagamento (String bandeiraCartao, double numeroCartao, int codigoSeguranca, String tipoPagamento) {
+	public FormaDePagamento (String bandeiraCartao, String numeroCartao, String codigoSeguranca, String tipoPagamento) {
 		this.bandeiraCartao = bandeiraCartao;
 		this.numeroCartao = numeroCartao;
 		this.codigoSeguranca = codigoSeguranca;
@@ -39,19 +39,19 @@ public class FormaDePagamento {
 		this.bandeiraCartao = bandeiraCartao;
 	}
 	
-	public double getNumeroCartao() {
+	public String getNumeroCartao() {
 		return numeroCartao;
 	}
 	
-	public void setNumeroCartao(double numeroCartao) {
+	public void setNumeroCartao(String numeroCartao) {
 		this.numeroCartao = numeroCartao;
 	}
 	
-	public int getCodigoSeguranca() {
+	public String getCodigoSeguranca() {
 		return codigoSeguranca;
 	}
 	
-	public void setCodigoSeguranca(int codigoSeguranca) {
+	public void setCodigoSeguranca(String codigoSeguranca) {
 		this.codigoSeguranca = codigoSeguranca;
 	}
 	
@@ -71,14 +71,14 @@ public class FormaDePagamento {
 		System.out.println("Cadastrar bandeira do cartao (Visa,Mastercard,Elo):");
 		setBandeiraCartao(ler.next());
 		respostaBandeira = getBandeiraCartao();
-		  if(respostaBandeira != "Visa" || respostaBandeira != "Mastercard" || respostaBandeira != "Elo") {
+		  if(respostaBandeira.contains ("Visa") || respostaBandeira.contains ("Mastercard") || respostaBandeira.contains ("Elo")) {
 			  validaBandeira = false;
 			  do {
 			  		System.out.println("Erro, so aceitamos as seguintes bandeiras" + "Visa, " + "MasterCard, " + "Elo. ");
 			  		System.out.println("Cadastrar bandeira do cartao (Visa,Mastercard,Elo):");
 					setBandeiraCartao(ler.next());
 					respostaBandeira = getBandeiraCartao();
-					  if(respostaBandeira != "Visa" || respostaBandeira != "Mastercard" || respostaBandeira != "Elo") {
+					  if(respostaBandeira.contains ("Visa") || respostaBandeira.contains ("Mastercard") || respostaBandeira.contains ("Elo")) {
 						  validaBandeira = false;
 					  }  
 			  }while(validaBandeira == false); //Ficarei nesse looping ate colocar uma bandeira valida		
@@ -89,7 +89,7 @@ public class FormaDePagamento {
 		System.out.println("Forma de Pagamento(debito, credito): ");
 		setTipoPagamento(ler.next());
 		respostaPagamento = getTipoPagamento();
-			if(respostaPagamento != "debito" || respostaPagamento != "Credito") { //So aceitaremos esses dois e escritos assim
+			if(respostaPagamento.contains ("debito") || respostaPagamento.contains ("Credito")) { //So aceitaremos esses dois e escritos assim
 				validaPagamento = false;
 				do {
 					System.out.println("Erro, so aceitamos credito ou debito, escritos dessa forma.");
@@ -97,46 +97,70 @@ public class FormaDePagamento {
 					System.out.println("Forma de Pagamento(debito, credito): ");
 					setTipoPagamento(ler.next());
 					respostaPagamento = getTipoPagamento();
-						if(respostaPagamento != "debito" || respostaPagamento != "Credito") { //So aceitaremos esses dois e escritos assim
+						if(respostaPagamento.contains ("debito") || respostaPagamento.contains ("Credito")) { //So aceitaremos esses dois e escritos assim
 							validaPagamento = false;
 						}
-				}while(validaPagamento == false); //Ficarei nesse looping ate por uma resposta valida
+				}while(validaPagamento == false); //Ficarei nesse looping ate pelo menos 16 digitos
 			}
 			
 		//Validando numero do cartao
 		System.out.println("Cadastrar numero do cartao para pagamento: ");
-		setNumeroCartao(ler.nextDouble());
+		setNumeroCartao(ler.next());
 		respostaNumero = getNumeroCartao();
-			//Erro pq length so pd ser p string e nao sei como fazer agr so com Double
-			if(respostaNumero.length() != 16) {
+			if(respostaNumero.length() != 16) { //Verificando se tenho 16 digitos
 				validaNumero = false;
 				do {
 					System.out.println("Erro, um cartao precisa ter 16 numeros.");
 					System.out.println("Cadastrar numero do cartao para pagamento: ");
-					setNumeroCartao(ler.nextDouble());
+					setNumeroCartao(ler.next());
 					respostaNumero = getNumeroCartao();
-						//Erro pq length so pd ser p string e nao sei como fazer agr so com Double
 						if(respostaNumero.length() != 16) {
 							validaNumero = false;
 						}
 				}while(validaNumero == false); //Ficarei nesse looping ate por uma resposta valida
 			}
+			if(!respostaNumero.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+				validaNumero = false;
+				do {
+					System.out.println("Erro, um cartao precisa ter 16 numeros.");
+					System.out.println("Cadastrar numero do cartao para pagamento: ");
+					setNumeroCartao(ler.next());
+					respostaNumero = getNumeroCartao();
+					if(!respostaNumero.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+						validaNumero = false;
+					}
+				}while(validaNumero == false); //Ficarei nesse looping ate digitar apenas numeros
+			}
 		
 		//Validando codigo de seguranca
 		System.out.println("Cadastrar codigo de seguranca do cartao: ");
-		setCodigoSeguranca(ler.nextInt());
+		setCodigoSeguranca(ler.next());
 		respostaSeguranca = getCodigoSeguranca();
-		if(respostaSeguranca >= 1000 || respostaSeguranca < 100) { //Logica para termos aqueles 3 numeros do codigo de seguranca
+		if(respostaSeguranca.length() != 3) { //Verificando que temos 3 digitos
 			validaCodigo = false;
 			do {
-				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos e maiores ou iguais a 100");
+				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos");
 				System.out.println("Cadastrar codigo de seguranca do cartao: ");
-				setCodigoSeguranca(ler.nextInt());
+				setCodigoSeguranca(ler.next());
 				respostaSeguranca = getCodigoSeguranca();
-				if(respostaSeguranca >= 1000 || respostaSeguranca < 100) { //Logica para termos aqueles 3 numeros do codigo de seguranca
+				if(respostaSeguranca.length() != 3) { //Verificando que temos 3 digitos
 					validaCodigo = false;
 				}
-			}while(validaCodigo == false); //Ficarei nesse looping ate por uma resposta valida.
+			}while(validaCodigo == false); //Ficarei nesse looping ate por pelo menos 3 digitos
+			
+		}
+		if(!respostaSeguranca.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+			validaCodigo = false;
+			do {
+				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos");
+				System.out.println("Cadastrar codigo de seguranca do cartao: ");
+				setCodigoSeguranca(ler.next());
+				respostaSeguranca = getCodigoSeguranca();
+				if(!respostaSeguranca.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+					validaCodigo = false;
+				}
+				
+			}while(validaCodigo == false); //Ficarei nesse looping ate por apenas digitos
 			
 		}
 		System.out.println("Cartao cadastrado com sucesso");
@@ -144,90 +168,116 @@ public class FormaDePagamento {
 	public void editarFormaPagamento() {
 		//Editar os cartoes cadastrados
 		
-
 		//Validando Bandeira do cartao, so aceitaremos esses 3 cartoes escritos nessa forma.
-		System.out.println("Cadastrar bandeira do cartao (Visa,Mastercard,Elo):");
+		System.out.println("Cadastrar nova bandeira do cartao (Visa,Mastercard,Elo):");
 		setBandeiraCartao(ler.next());
 		respostaBandeira = getBandeiraCartao();
-		  if(respostaBandeira != "Visa" || respostaBandeira != "Mastercard" || respostaBandeira != "Elo") {
+		  if(respostaBandeira.contains ("Visa") || respostaBandeira.contains ("Mastercard") || respostaBandeira.contains ("Elo")) {
 			  validaBandeira = false;
 			  do {
 			  		System.out.println("Erro, so aceitamos as seguintes bandeiras" + "Visa, " + "MasterCard, " + "Elo. ");
-			  		System.out.println("Cadastrar bandeira do cartao (Visa,Mastercard,Elo):");
+			  		System.out.println("Cadastrar nova bandeira do cartao (Visa,Mastercard,Elo):");
 					setBandeiraCartao(ler.next());
 					respostaBandeira = getBandeiraCartao();
-					  if(respostaBandeira != "Visa" || respostaBandeira != "Mastercard" || respostaBandeira != "Elo") {
+					  if(respostaBandeira.contains ("Visa") || respostaBandeira.contains ("Mastercard") || respostaBandeira.contains ("Elo")) {
 						  validaBandeira = false;
 					  }  
 			  }while(validaBandeira == false); //Ficarei nesse looping ate colocar uma bandeira valida		
 		  }
 		
 		//Validando forma de pagamento  
-		System.out.println("Cadastrar forma de pagamento: ");
+		System.out.println("Cadastrar  nova forma de pagamento: ");
 		System.out.println("Forma de Pagamento(debito, credito): ");
 		setTipoPagamento(ler.next());
 		respostaPagamento = getTipoPagamento();
-			if(respostaPagamento != "debito" || respostaPagamento != "Credito") { //So aceitaremos esses dois e escritos assim
+			if(respostaPagamento.contains ("debito") || respostaPagamento.contains ("Credito")) { //So aceitaremos esses dois e escritos assim
 				validaPagamento = false;
 				do {
 					System.out.println("Erro, so aceitamos credito ou debito, escritos dessa forma.");
-					System.out.println("Cadastrar forma de pagamento: ");
+					System.out.println("Cadastrar nova forma de pagamento: ");
 					System.out.println("Forma de Pagamento(debito, credito): ");
 					setTipoPagamento(ler.next());
 					respostaPagamento = getTipoPagamento();
-						if(respostaPagamento != "debito" || respostaPagamento != "Credito") { //So aceitaremos esses dois e escritos assim
+						if(respostaPagamento.contains ("debito") || respostaPagamento.contains ("Credito")) { //So aceitaremos esses dois e escritos assim
 							validaPagamento = false;
 						}
-				}while(validaPagamento == false); //Ficarei nesse looping ate por uma resposta valida
+				}while(validaPagamento == false); //Ficarei nesse looping ate pelo menos 16 digitos
 			}
 			
 		//Validando numero do cartao
-		System.out.println("Cadastrar numero do cartao para pagamento: ");
-		setNumeroCartao(ler.nextDouble());
+		System.out.println("Cadastrar novo numero do cartao para pagamento: ");
+		setNumeroCartao(ler.next());
 		respostaNumero = getNumeroCartao();
-			//Erro pq length so pd ser p string e nao sei como fazer agr so com Double
-			if(respostaNumero.length() != 16) {
+			if(respostaNumero.length() != 16) { //Verificando se tenho 16 digitos
 				validaNumero = false;
 				do {
 					System.out.println("Erro, um cartao precisa ter 16 numeros.");
-					System.out.println("Cadastrar numero do cartao para pagamento: ");
-					setNumeroCartao(ler.nextDouble());
+					System.out.println("Cadastrar novo numero do cartao para pagamento: ");
+					setNumeroCartao(ler.next());
 					respostaNumero = getNumeroCartao();
-						//Erro pq length so pd ser p string e nao sei como fazer agr so com Double
 						if(respostaNumero.length() != 16) {
 							validaNumero = false;
 						}
 				}while(validaNumero == false); //Ficarei nesse looping ate por uma resposta valida
 			}
+			if(!respostaNumero.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+				validaNumero = false;
+				do {
+					System.out.println("Erro, um cartao precisa ter 16 numeros.");
+					System.out.println("Cadastrar  novo numero do cartao para pagamento: ");
+					setNumeroCartao(ler.next());
+					respostaNumero = getNumeroCartao();
+					if(!respostaNumero.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+						validaNumero = false;
+					}
+				}while(validaNumero == false); //Ficarei nesse looping ate digitar apenas numeros
+			}
 		
 		//Validando codigo de seguranca
 		System.out.println("Cadastrar codigo de seguranca do cartao: ");
-		setCodigoSeguranca(ler.nextInt());
+		setCodigoSeguranca(ler.next());
 		respostaSeguranca = getCodigoSeguranca();
-		if(respostaSeguranca >= 1000 || respostaSeguranca < 100) { //Logica para termos aqueles 3 numeros do codigo de seguranca
+		if(respostaSeguranca.length() != 3) { //Verificando que temos 3 digitos
 			validaCodigo = false;
 			do {
-				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos e maiores ou iguais a 100");
-				System.out.println("Cadastrar codigo de seguranca do cartao: ");
-				setCodigoSeguranca(ler.nextInt());
+				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos");
+				System.out.println("Cadastrar novo codigo de seguranca do cartao: ");
+				setCodigoSeguranca(ler.next());
 				respostaSeguranca = getCodigoSeguranca();
-				if(respostaSeguranca >= 1000 || respostaSeguranca < 100) { //Logica para termos aqueles 3 numeros do codigo de seguranca
+				if(respostaSeguranca.length() != 3) { //Verificando que temos 3 digitos
 					validaCodigo = false;
 				}
-			}while(validaCodigo == false); //Ficarei nesse looping ate por uma resposta valida.
+			}while(validaCodigo == false); //Ficarei nesse looping ate por pelo menos 3 digitos
+			
+		}
+		if(!respostaSeguranca.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+			validaCodigo = false;
+			do {
+				System.out.println("Erro, um codigo de seguranca precisa ter 3 digitos");
+				System.out.println("Cadastrar  novo codigo de seguranca do cartao: ");
+				setCodigoSeguranca(ler.next());
+				respostaSeguranca = getCodigoSeguranca();
+				if(!respostaSeguranca.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+					validaCodigo = false;
+				}
+				
+			}while(validaCodigo == false); //Ficarei nesse looping ate por apenas digitos
 			
 		}
 		System.out.println("Cartao editado com sucesso");
 	}
 	public void deletarFormaPagamento() { 
 		// Escolher qual carta e Resetar todos os dados fazendo virar null.
-		setTipoPagamento("");
-		setNumeroCartao(0);
-		setBandeiraCartao("");
-		setCodigoSeguranca(0);
+		setTipoPagamento(null);
+		setNumeroCartao(null);
+		setBandeiraCartao(null);
+		setCodigoSeguranca(null);
 	}
 	public void atualizarFormaPgamento() {
 		//Atualizar todos os cartoes que forem editados e cadastrados
-		
+		System.out.println("Bandeira: "+ getBandeiraCartao());
+		System.out.println("Tipo de pagamento: "+ getTipoPagamento());
+		System.out.println("Numero do cartao: "+ getNumeroCartao());
+		System.out.println("Codigo de seguranca: "+ getCodigoSeguranca());
 	}
 }
