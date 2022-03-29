1456b164eaf51b0f6c98dadcebb95da53f3d58db
Lucas <lucasfs1007@gmail.com>
2022-03-29 12:48:23 -0300

    Corrigindo alguns erros e iniciando a lógica do checkin

--- TrabalhoPratico/src/Classes/Tempo.java
+++ TrabalhoPratico/src/Classes/Tempo.java
@@ -6,33 +6,40 @@ import java.util.Date;
 public class Tempo {
 	//Tempo vai herdar o tempo final para pedido para termos o valor final
 	//Atributos
-	private double horaCheckin;
-	private double horaCheckout;
-	private double tempoTotal;
+	private String horaCheckin;
+	private String horaCheckout;
+	private String tempoTotal;
 	
+	//Variaveis extra
+	String respostaCheckin;
+	String respostaCheckout;
+	Boolean validaCheckin = true;
+	Boolean validaCheckout = true;  
+	
+	Scanner resposta = new Scanner(System.in); //Leitura de dados
 	//Metodo construtor do Tempo
-	public Tempo(double horaCheckin, double horaCheckout, double tempoTotal) {
+	public Tempo(String horaCheckin, String horaCheckout, String tempoTotal) {
 		this.horaCheckin = horaCheckin;
-		this.horaCheckin = horaCheckout;
+		this.horaCheckout = horaCheckout;
 		this.tempoTotal = tempoTotal;
   	}
 	
-	public double getHoraCheckIn() {
+	public String getHoraCheckIn() {
 		return horaCheckin;
 	}
-	public void setHoraCheckIn(double horaCheckIn) {
+	public void setHoraCheckIn(String horaCheckIn) {
 		this.horaCheckin = horaCheckIn;
 	}
-	public double getHoraCheckOut() {
+	public String getHoraCheckOut() {
 		return horaCheckout;
 	}
-	public void setHoraCheckOut(double horaCheckOut) {
+	public void setHoraCheckOut(String horaCheckOut) {
 		this.horaCheckout = horaCheckOut;
 	}
-	public double getTempoTotal() {
+	public String getTempoTotal() {
 		return tempoTotal;
 	}
-	public void setTempoTotal(double tempoTotal) {
+	public void setTempoTotal(String tempoTotal) {
 		this.tempoTotal = tempoTotal;
 	}
 	
@@ -41,14 +48,23 @@ public class Tempo {
 		//Cadastrar a hora de checkin e checkout e com isso ter o tempo final.
 		//Utilizaremos o tempo final pro pedido pra gerar o pedido com valor por hora
 		
-		Scanner resposta = new Scanner(System.in); //Leitura de dados
-		//Variaveis pra brincar com a logica apenas, pq tenho que achar como fazer as funcoes de hora mais funcionais
-		 double checkinProvisorio = 0;
-		 double checkoutProvisorio = 0;
-		 double tempoTotalProvisorio = 0;
-		 
-		System.out.println("Insira seu horario de checkin: ");
-		checkinProvisorio = resposta.nextDouble();
+		//Validando hora Check-in
+		System.out.println("Insira a hora de check-in: ");
+		setHoraCheckIn(resposta.next());
+		respostaCheckin = getHoraCheckIn();
+		if(!respostaCheckin.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+			validaCheckin = false;
+			do {
+				System.out.println("Digite uma hora valida para seu check-in.");
+				System.out.println("Insira a hora de check-in: ");
+				setHoraCheckIn(resposta.next());
+				respostaCheckin = getHoraCheckIn();
+				if(!respostaCheckin.substring(0).matches("[0-9]*")) { //Verificando se tenho apenas numeros
+					validaCheckin = false;
+				}
+			}while(validaCheckin == false); //Ficarei nesse looping ate digitar somente numeros
+		}
+		
 	}
 	public void editarTempo() {
 		 //Editar hora de checkin e checkout e consequentemente o tempo final
