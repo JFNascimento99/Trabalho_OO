c5eb7f476d54389657849b203058822a3eb969a5
Lucas <lucasfs1007@gmail.com>
2022-04-06 10:01:19 -0300

    Criando a tela de visualizar cartões

---
+++ TrabalhoPratico/src/View/VisualizaPagamento.java
@@ -0,0 +1,62 @@
+package View;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import Classes.Dados;
+import Classes.FormaDePagamento;
+
+//Fazer antes uma tela de seleção de usuário
+
+public class TelaVisualizaPagamento implements ActionListener {
+
+	private JFrame janela = new JFrame("Menu de Visualizacao");
+	private JLabel titulo = new JLabel("Visualizar cartoes cadastrados");
+
+
+	private static JButton ver = new JButton("ver");
+
+	public TelaVisualizaPagamento() {
+		titulo.setFont(new Font("Arial", Font.BOLD, 20)); // definindo fonte do t�tulo
+		// definindo as medidas dos botões
+		// Legenda: X, Y, Largura, Altura
+		titulo.setBounds(145, 10, 200, 30);
+		ver.setBounds(360, 400, 100, 30);
+
+		janela.setLayout(null);
+		// chamando as informação que estarão na janela
+		janela.add(titulo);
+		janela.add(ver);
+
+		// Setando dimensões e funções padrão da janela
+		janela.setSize(480, 480);
+		janela.setVisible(true);
+
+		// Retorna o texto
+
+		janela.setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		TelaVisualizaPagamento menu = new TelaVisualizaPagamento();
+		System.out.println("teste");
+		ver.addActionListener(menu);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		// Quando o botão concluir for presionado, deve pegar os valores nos getText e
+		// colocar nos respectivos lugares
+		if (src == ver) {
+			("Cartoes : \n" + Dados.getFormaDePagamento());
+			
+
+	}
+			}
+
+			// Fazer lógica para alterals os valores
+		
+	}
+
+
