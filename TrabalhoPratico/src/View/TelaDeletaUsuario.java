b30e0693a1a7c50d53134f82e880a53d6d2fee31
Lucas <lucasfs1007@gmail.com>
2022-04-05 21:23:14 -0300

    Merge branch 'master' of https://github.com/JFNascimento99/Trabalho_OO.git

---
+++ TrabalhoPratico/src/View/TelaDeletaUsuario.java
@@ -0,0 +1,124 @@
+package View;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import Classes.Dados;
+import Classes.Usuario;
+
+//Fazer antes uma tela de seleção de usuário
+
+public class TelaDeletaUsuario implements ActionListener {
+
+	private JFrame janela = new JFrame("Menu de Cadastro");
+	private JLabel titulo = new JLabel("Deletar usuário");
+
+	// Declarando Labels e caixas de texto
+	private JLabel nome = new JLabel("Nome");
+	private JTextField caixaNome = new JTextField();
+
+	private JLabel cpf = new JLabel("CPF");
+	private JTextField caixaCpf = new JTextField(15);
+
+	private JLabel rg = new JLabel("RG");
+	private JTextField caixaRg = new JTextField(15);
+
+	private JLabel telefone = new JLabel("Tel.");
+	private JTextField caixaTelefone = new JTextField(15);
+
+	private JLabel idade = new JLabel("Idade");
+	private JTextField caixaIdade = new JTextField(15);
+
+	private JLabel email = new JLabel("Email");
+	private JTextField caixaEmail = new JTextField(15);
+
+	private JLabel senha = new JLabel("Senha");
+	private JTextField caixaSenha = new JTextField(15);
+
+	private static JButton deletar = new JButton("deletar");
+	private static JButton confirmar = new JButton("Confirmar");
+
+	public TelaDeletaUsuario() {
+		titulo.setFont(new Font("Arial", Font.BOLD, 20)); // definindo fonte do t�tulo
+		// definindo as medidas dos botões
+		// Legenda: X, Y, Largura, Altura
+		titulo.setBounds(145, 10, 200, 30);
+
+		nome.setBounds(100, 50, 180, 30);
+		caixaNome.setBounds(200, 50, 180, 30);
+
+		cpf.setBounds(100, 100, 180, 30);
+		caixaCpf.setBounds(200, 100, 180, 30);
+
+		rg.setBounds(100, 150, 180, 30);
+		caixaRg.setBounds(200, 150, 180, 30);
+
+		telefone.setBounds(100, 200, 180, 30);
+		caixaTelefone.setBounds(200, 200, 180, 30);
+
+		idade.setBounds(100, 250, 180, 30);
+		caixaIdade.setBounds(200, 250, 180, 30);
+
+		email.setBounds(100, 300, 180, 30);
+		caixaEmail.setBounds(200, 300, 180, 30);
+
+		senha.setBounds(100, 350, 180, 30);
+		caixaSenha.setBounds(200, 350, 180, 30);
+
+		confirmar.setBounds(360, 400, 100, 30);
+
+		janela.setLayout(null);
+		// chamando as informação que estarão na janela
+		janela.add(titulo);
+		janela.add(nome);
+		janela.add(caixaNome);
+		janela.add(cpf);
+		janela.add(caixaCpf);
+		janela.add(rg);
+		janela.add(caixaRg);
+		janela.add(telefone);
+		janela.add(caixaTelefone);
+		janela.add(idade);
+		janela.add(caixaIdade);
+		janela.add(email);
+		janela.add(caixaEmail);
+		janela.add(senha);
+		janela.add(caixaSenha);
+		janela.add(deletar);
+		janela.add(confirmar);
+
+		// Setando dimensões e funções padrão da janela
+		janela.setSize(480, 480);
+		janela.setVisible(true);
+
+		// Retorna o texto
+		// Ver como fazer a l�gica de armazenar
+		caixaNome.getText();
+		caixaCpf.getText();
+		caixaRg.getText();
+		caixaTelefone.getText();
+		caixaIdade.getText();
+		caixaEmail.getText();
+		caixaSenha.getText();
+
+		janela.setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		TelaCadastroUsuario menu = new TelaCadastroUsuario();
+		System.out.println("teste");
+		confirmar.addActionListener(menu);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		// Quando o botão concluir for presionado, deve pegar os valores nos getText e
+		// colocar nos respectivos lugares
+		if (src == confirmar) {
+			Dados.getUsuarios().remove(this); 
+			System.out.println("Usuario deletado.");
+		}
+				} 
+		
+	}
